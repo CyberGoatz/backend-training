1a7375685427c08da0103f6a2d6dfc36139d427c
Kartik213 <kartikg02013@gmail.com>
2026-05-11 16:37:32 +0530

    feat(training): allow trainees to read run answers

--- training-service/src/main/java/cz/cyberrange/platform/training/service/facade/TrainingRunFacade.java
+++ training-service/src/main/java/cz/cyberrange/platform/training/service/facade/TrainingRunFacade.java
@@ -34,6 +34,7 @@ import cz.cyberrange.platform.training.persistence.model.TrainingRun;
 import cz.cyberrange.platform.training.persistence.model.enums.TRState;
 import cz.cyberrange.platform.training.persistence.model.question.QuestionAnswer;
 import cz.cyberrange.platform.training.service.annotations.security.IsOrganizerOrAdmin;
+import cz.cyberrange.platform.training.service.annotations.security.IsOrganizerOrAdminOrTraineeOfTrainingRun;
 import cz.cyberrange.platform.training.service.annotations.security.IsTrainee;
 import cz.cyberrange.platform.training.service.annotations.security.IsTraineeOrAdmin;
 import cz.cyberrange.platform.training.service.annotations.transactions.TransactionalRO;
@@ -546,7 +547,7 @@ public class TrainingRunFacade {
      * @param trainingRunId id of Training Run which current level gets hint for.
      * @return {@link CorrectAnswerDTO[]}
      */
-    @IsOrganizerOrAdmin
+    @IsOrganizerOrAdminOrTraineeOfTrainingRun
     @TransactionalWO
     public List<CorrectAnswerDTO> getCorrectAnswers(Long trainingRunId) {
         TrainingRun trainingRun = trainingRunService.findByIdWithLevel(trainingRunId);
